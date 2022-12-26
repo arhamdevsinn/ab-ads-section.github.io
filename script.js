ef37c188bbe656a0de208babc8fe21f27bccb9aa
AbuzarZaidi <abuzarzaidi947@gmail.com>
2022-12-26 21:21:09 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,90 @@
+var myFP = fluidPlayer(
+    'video-id',	{
+"layoutControls": {
+    "controlBar": {
+        "autoHideTimeout": 3,
+        "animated": true,
+        "autoHide": true
+    },
+    "htmlOnPauseBlock": {
+        "html": null,
+        "height": null,
+        "width": null
+    },
+    "autoPlay": false,
+    "mute": true,
+    "allowTheatre": true,
+    "playPauseAnimation": true,
+    "playbackRateEnabled": false,
+    "allowDownload": false,
+    "playButtonShowing": true,
+    "fillToContainer": false,
+    "posterImage": ""
+},
+"vastOptions": {
+    "adList": [
+        {
+            "roll": "preRoll",
+            "vastTag": "https://www.videosprofitnetwork.com/watch.xml?key=a4dd62122375839e590895d5cee4a7ca",
+            "adText": ""
+        },
+        {
+            "roll": "preRoll",
+            "vastTag": "https://www.videosprofitnetwork.com/watch.xml?key=a4dd62122375839e590895d5cee4a7ca",
+            "adText": ""
+        },
+        {
+            "roll": "preRoll",
+            "vastTag": "https://www.videosprofitnetwork.com/watch.xml?key=a4dd62122375839e590895d5cee4a7ca",
+            "adText": ""
+        },
+        {
+            "roll": "preRoll",
+            "vastTag": "https://www.videosprofitnetwork.com/watch.xml?key=a4dd62122375839e590895d5cee4a7ca",
+            "adText": ""
+        },
+        {
+            "roll": "preRoll",
+            "vastTag": "https://www.videosprofitnetwork.com/watch.xml?key=a4dd62122375839e590895d5cee4a7ca",
+            "adText": ""
+        },
+        {
+            "roll": "preRoll",
+            "vastTag": "https://www.videosprofitnetwork.com/watch.xml?key=a4dd62122375839e590895d5cee4a7ca",
+            "adText": ""
+        },
+        {
+            "roll": "preRoll",
+            "vastTag": "https://www.videosprofitnetwork.com/watch.xml?key=a4dd62122375839e590895d5cee4a7ca",
+            "adText": ""
+        },
+        {
+            "roll": "preRoll",
+            "vastTag": "https://www.videosprofitnetwork.com/watch.xml?key=a4dd62122375839e590895d5cee4a7ca",
+            "adText": ""
+        },
+        {
+            "roll": "preRoll",
+            "vastTag": "https://www.videosprofitnetwork.com/watch.xml?key=a4dd62122375839e590895d5cee4a7ca",
+            "adText": ""
+        },
+        {
+            "roll": "preRoll",
+            "vastTag": "https://www.videosprofitnetwork.com/watch.xml?key=a4dd62122375839e590895d5cee4a7ca",
+            "adText": ""
+        },
+        {
+            "roll": "midRoll",
+            "vastTag":  "https://www.videosprofitnetwork.com/watch.xml?key=a4dd62122375839e590895d5cee4a7ca",
+            "adText": ""
+        },
+        {
+            "roll": "postRoll",
+            "vastTag":  "https://www.videosprofitnetwork.com/watch.xml?key=a4dd62122375839e590895d5cee4a7ca",
+            "adText": ""
+        }
+    ],
+    "adCTAText": false,
+    "adCTATextPosition": ""
+}
+});
